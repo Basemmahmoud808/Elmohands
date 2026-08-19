7ef58d308f2c21749bd51b19a2197a820eabf33e
Almohands Team <almohands@example.com>
2026-08-19 11:26:07 +0300

    feat(ui): Remove Sentry floating Report a Bug feedback widget

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,13 +2,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://609c4584a9ccbb231fb13fb886b92b7d@o4511859034882048.ingest.de.sentry.io/4511859038224464",
-  integrations: [
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
-      isNameRequired: true,
-      isEmailRequired: true,
-    }),
-  ],
   tracesSampleRate: 1.0,
 });
